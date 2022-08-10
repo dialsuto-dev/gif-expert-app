da38a00c61565d6b6645f2064177c9d8c94a4575
Diego Suarez <diego.suarez@mercadolibre.com.co>
2022-08-10 16:45:53 -0500

    first commit

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,17 @@
+export const getGifs = async (category) => {
+  const apiKey = 'yox6Te3KgEcAPqws5QAdTGUjNn30pA8Q';
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${category}&limit=10`;
+  const resp = await fetch(url);
+
+  const {data = []} = await resp.json();
+
+  const gifs = data.map(img => (
+    {
+      id: img.id,
+      title: img.title,
+      url: img.images.downsized_medium.url
+    }
+  ));
+
+  return gifs;
+};
